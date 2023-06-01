9ca66320b1e5516ec249a617cfb475d9e19e34a3
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-06-01 02:19:54 +0000

    Add

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
   has_many :book_comment, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
